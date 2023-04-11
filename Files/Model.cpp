e11126f54a87ed11f31aa5e46e26b1abd4cdc827
Alessandro Trasatti <alessandro.trasatti2@gmail.com>
2023-04-11 19:36:23 +0200

    Commented and debugged Model.h and Model.cpp.

--- Files/Model.cpp
+++ Files/Model.cpp
@@ -1,44 +1,38 @@
 #include "Model.h"
 
+// Implementation of the methods for the Model class
 Model::Model(const double & initial_value)
 	: _initial_value(initial_value)
 {
 }
-
 Model::Model(const Model & model)
 	: _initial_value(model._initial_value)
 {
 }
-
 Model & Model::operator=(const Model & model)
 {
 	if (this != &model)
 		_initial_value = model._initial_value;
 	return *this;
 }
-
-
-
 double Model::init_value() const
 {
 	return _initial_value;
 }
 
+// Implementation of the methods for the BlackScholesModel class
 BlackScholesModel::BlackScholesModel(const double & initial_value, const double & drift, const double & volatility)
 	: Model(initial_value), _drift(drift), _volatility(volatility)
 {
 }
-
 BlackScholesModel::BlackScholesModel(const BlackScholesModel & model)
 	: Model(model), _drift(model._drift), _volatility(model._volatility)
 {
 }
-
 BlackScholesModel * BlackScholesModel::clone() const
 {
 	return new BlackScholesModel(*this);
 }
-
 BlackScholesModel & BlackScholesModel::operator=(const BlackScholesModel & model)
 {
 	if (this != &model)
@@ -49,33 +43,34 @@ BlackScholesModel & BlackScholesModel::operator=(const BlackScholesModel & model
 	}
 	return *this;
 }
-
 double BlackScholesModel::drift_term(const double & time, const double & asset_price) const
 {
 	return _drift * asset_price;
 }
-
 double BlackScholesModel::diffusion_term(const double & time, const double & asset_price) const
 {
 	return _volatility * asset_price;
 }
 
+// Implementation of the methods for the HestonModel class
+
+// THE FOLLOWING METHOD IS NOT RIGHT AND NEEDS TO BE CHANGED
 HestonModel::HestonModel(const double& initial_spot, const double& initial_variance, const double& drift, const double& mean_reversion_speed, const double& mean_reversion_level, const double& vol_of_vol, const double& correlation)
 	: Model(initial_spot), _initial_variance(initial_variance), _drift(drift), _mean_reversion_speed(mean_reversion_speed), _mean_reversion_level(mean_reversion_level), _vol_of_vol(vol_of_vol), _correlation(correlation)
 {
 }
-
+// THE FOLLOWING METHOD IS NOT RIGHT AND NEEDS TO BE CHANGED
 HestonModel::HestonModel(const HestonModel& model)
 	: Model(model._initial_spot), _initial_variance(model._initial_variance), _drift(model._drift), _mean_reversion_speed(model._mean_reversion_speed), _mean_reversion_level(model._mean_reversion_level), _vol_of_vol(model._vol_of_vol), _correlation(model._correlation)
 {
 }
-
+// THE FOLLOWING METHOD IS NOT RIGHT AND NEEDS TO BE CHANGED
 HestonModel& HestonModel::operator=(const HestonModel& model)
 {
 	if (this != &model) {
 		Model::operator=(model);
 		_initial_variance = model._initial_variance;
-		_drif = model._drift;
+		_drift = model._drift;
 		_mean_reversion_speed = model._mean_reversion_speed;
 		_mean_reversion_level = model._mean_reversion_level;
 		_vol_of_vol = model._vol_of_vol;
@@ -83,37 +78,30 @@ HestonModel& HestonModel::operator=(const HestonModel& model)
 	}
 	return *this;
 }
-
 double HestonModel::initial_spot() const
 {
 	return _initial_spot;
 }
-
 double HestonModel::initial_variance() const
 {
 	return _initial_variance;
 }
-
 double HestonModel::drift() const
 {
 	return _drift;
 }
-
 double HestonModel::mean_reversion_speed() const
 {
 	return _mean_reversion_speed;
 }
-
 double HestonModel::mean_reversion_level() const
 {
 	return _mean_reversion_level;
 }
-
 double HestonModel::vol_of_vol() const
 {
 	return _vol_of_vol;
 }
-
 double HestonModel::correlation() const
 {
 	return _correlation;
--- Files/Model.h
+++ Files/Model.h
@@ -1,49 +1,53 @@
 #pragma once
 
-// An abstract class is a class which contains at least one virtual pure method
+// Defines an abstract class of a general diffusive model
 class Model
 {
 public:
+	// Parameter constructor
 	Model(const double& initial_value);
 
 	// Copy constructor
 	Model(const Model& model);
 
-	// clone method -> abstract class that is used by another class through a pointer
+	// Clone method - This needs to be implemented as Model is an abstract class and the class PathSimulator has a pointer to an object of type model as attribute 
 	virtual Model* clone() const = 0;
 
 	// Assignment operator
 	Model& operator=(const Model& model);
 
-	// Destructor
+	// Destructor - Bound to be virtual as other classes will inherit from this
 	virtual ~Model() = default;
 
-	// getter
+	// getter of S0
 	double init_value() const;
 
+	// \mu(t,S_t)
 	virtual double drift_term(const double& time, const double& asset_price) const = 0;
+
+	// \sigma(t,S_t)
 	virtual double diffusion_term(const double& time, const double& asset_price) const = 0;
 
 protected:
 	double _initial_value;
 };
 
-// Public Inheritance :
-// public -> public
-// protected -> private 
-// private -> unaccessible
-
 class BlackScholesModel : public Model
 {
 public:
+	// Parameter constructor
 	BlackScholesModel(const double& initial_value, const double& drift, const double& volatility);
 
+	// Copy constructor
 	BlackScholesModel(const BlackScholesModel& model);
 
+	// Clone method
 	BlackScholesModel* clone() const override;
 
+	// Assignment operator
 	BlackScholesModel& operator=(const BlackScholesModel& model);
 
+	// Destructor
 	~BlackScholesModel() = default;
 
 	double drift_term(const double& time, const double& asset_price) const override;
@@ -57,6 +61,7 @@ private:
 class HestonModel : public Model
 {
 public:
+	// Parameter constructor
 	HestonModel(
 		const double& initial_spot,
 		const double& initial_variance,
@@ -65,11 +70,19 @@ public:
 		const double& mean_reversion_level,
 		const double& vol_of_vol,
 		const double& correlation);
+	// Copy constructor
 	HestonModel(const HestonModel& model);
-	HestonModel* clone() const override;
+
+	// Clone method
+	HestonModel* clone() const override; // TO OVERRIDE WE NEED TO INHERIT THE CLASS
+
+	// Assignment operator
 	HestonModel& operator=(const HestonModel& model);
+
+	// Destructor
 	~HestonModel() = default;
-	// getter methods
+
+	// Getter methods
 	double initial_spot() const;
 	double initial_variance() const;
 	double drift() const;
@@ -78,6 +91,7 @@ public:
 	double vol_of_vol() const;
 	double correlation() const;
 private:
+	double _initial_spot; // S0
 	double _initial_variance; // V_0
 	double _drift; // rate r
 	double _mean_reversion_speed; // kappa
