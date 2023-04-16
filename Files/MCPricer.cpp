963ec670b68f4c3e53dd1ff3a0e727692e6c4348
Valentin Paravy <valentin.paravy@gmail.com>
2023-04-16 10:01:36 +0200

    changes in the pricer, instead of having a price that diverges if one path diverges now we warn the user instead and compute the price among the paths that didn't diverge

--- Files/MCPricer.cpp
+++ Files/MCPricer.cpp
@@ -36,18 +36,32 @@ double MCPricer::price() const
 	// price = DF(0,T) * E[ phi((S_t))] 
 	// E[phi((S_t))] = 1/N * SUM(phi(each path))
 
+	// if few trajectories diverge, instead of having a price that will diverge we don't consider them for the computation 
+	// but we warn the user and tell him how many trajectories diverged.
+	int n_nan = 0;
 	double price = 0.;
 	for (size_t sim_idx = 0; sim_idx < _number_sims; sim_idx++)
 	{
 		Matrix path = _pathSimulator->path();
-		price += _payoff->payoff(path);
+		n_nan = (isnan(path[path.size() - 1][0])) ? n_nan + 1 : n_nan;
+		price = (isnan(path[path.size() - 1][0])) ? price : price + _payoff->payoff(path);
 	}
-	price /= (double)_number_sims;
+	// Case if all paths are nan
+	if (n_nan == (double)_number_sims) {
+		throw "Every path gave a nan";
+	}
+	else {
+		price /= ((double)_number_sims - n_nan);
+	}
 
 	// Discount Factor
 	double disc_fact = std::exp(-_risk_free_rate * _pathSimulator->expiry());
 
 	price *= disc_fact;
+	
+	//We tell the user about the proportion of nan
+	std::cout << 100.0 * ((double)_number_sims - n_nan) / ((double)_number_sims);
+	std::cout << "% of the simulations were sucessful, the rest diverged" << std::endl;
 	return price;
 
 }
